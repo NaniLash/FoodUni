7b34891dd9e70157cfa37898e841dc362d152803
imR2D2 <arturo.hzb14@gmail.com>
2023-03-09 10:58:42 -0600

    yamero

--- flutter_application/lib/main.dart
+++ flutter_application/lib/main.dart
@@ -1,49 +1,65 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('FoodUni'),
-        ),
-        body: ListView(
-          children: [
-            Container(
-              padding: EdgeInsets.all(20.0),
-              child:Image.network("https://images.unsplash.com/photo-1611003228941-98852ba62227?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmFieSUyMGRvZ3xlbnwwfHwwfHw%3D&w=1000&q=80")
-            ),
-            Container(
-              height: 100,
-              color: Colors.amber[600],
-              child: const Center(child: Text('Entry AA')),
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // ignore: sized_box_for_whitespace
-                Container(
-                  child:Text("JIA "),
-                ),
-                // ignore: prefer_const_constructors
-                Text("Hola Mundo"),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text("Holaa Maaundo"),
-                Text("JIJIJIJA"),
-              ],
-            ),
-          ],
-        ),
+void main() {
+  runApp(
+    const MaterialApp(
+      title: "FoodUni",
+      //SafeArea respeta el contenido de la pantalla de Android
+      home: SafeArea(
+        child: MyApp()
       ),
+    )
+  );
+}
+
+class MyApp extends StatelessWidget { 
+  //Constructor
+  const MyApp ({super.key});
+  
+  @override
+  //El metodo build se usa para definir la apariencia y estructura de un widget
+  Widget build(BuildContext context) {
+    //Widget de material design
+    return Material(
+      child: Column(
+        children: [
+        
+          Container(
+            height: 50,
+            padding: const EdgeInsets.all(8.0),
+            decoration: const BoxDecoration(color: Colors.red),
+            child: Row(
+              children: const [
+                IconButton(
+                  onPressed: null,
+                  icon: Icon(Icons.menu),
+                  tooltip: 'Boton de Navegacion',
+                ),
+                Expanded(
+                  child: Text('App Hola Mundo')
+                ),
+                IconButton(
+                  onPressed: null, 
+                  icon: Icon(Icons.search), 
+                  tooltip: 'Boton de Busqueda',
+                ),
+              ],
+            ),
+          ),
+
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Hola Mundo', 
+                textDirection: TextDirection.ltr,
+              ),
+            ),
+          ),
+        ],
+      ), 
     );
   }
+
 }
+
+
